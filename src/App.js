bd985df401951bd20671fe0529ec2defe4dd030f
Alexej Zajtsev <alexej3ajtsev@gmail.com>
2019-11-27 00:04:29 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import BemHelper from 'react-bem-helper';
+import { ACCESS_KEY } from './index';
 import './App.css';
 
+const bem = new BemHelper('element-cover');
+
+const Item = ({ pictureUrl }) => {
+  return (
+    <div {...bem('blurred-picture-wrapper')}>
+      <div {...bem('blurred-bg')} style={{ backgroundImage: `url(${pictureUrl})` }}></div>
+      <img src={pictureUrl} alt="" />
+    </div>
+  );
+};
+
 function App() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchCats = async () => {
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query: 'cats' },
+        headers: {
+          Authorization: `Client-ID ${ACCESS_KEY}`
+        }
+      });
+      const {
+        data: { results: cats }
+      } = response;
+
+      const items = cats.reduce((acc, cur) => {
+        return [
+          ...acc,
+          {
+            pictureUrl: cur.urls.small
+          }
+        ];
+      }, []);
+
+      setItems(items);
+    };
+    fetchCats();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="layout-grid-wrap">
+        {items && items.map(item => <Item pictureUrl={item.pictureUrl} />)}
+      </div>
     </div>
   );
 }
